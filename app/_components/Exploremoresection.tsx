66d2dc4481a14954dfdaca2ab04ba37688aa5459
Pushkar Gupta <pushkargupta606@gmail.com>
2026-01-28 23:23:05 +0530

    Refactor Exploremoresection component to arrow function

--- app/_components/Exploremoresection.tsx
+++ app/_components/Exploremoresection.tsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import CourseList from '../(routes)/courses/_components/CourseList'
+import React from "react";
+import CourseList from "../(routes)/courses/_components/CourseList";
 
-function Exploremoresection() {
+const ExploreMoreSection = () => {
   return (
-    <section className="relative bg-black pt-10 pb-24 px-6">
-      
+    <section className="relative bg-black pt-10 pb-24 px-6 overflow-hidden">
       {/* Soft background glow */}
       <div className="absolute inset-0 -z-10 bg-gradient-to-br from-purple-500/10 via-pink-500/10 to-blue-500/10 blur-3xl" />
 
       <div className="max-w-7xl mx-auto">
-        
         {/* Section Heading */}
         <h2 className="font-game mb-10 text-3xl md:text-4xl text-center text-white">
           Explore Our <span className="text-yellow-400">Courses</span>
@@ -18,7 +16,7 @@ function Exploremoresection() {
         <CourseList />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Exploremoresection
+export default ExploreMoreSection;
